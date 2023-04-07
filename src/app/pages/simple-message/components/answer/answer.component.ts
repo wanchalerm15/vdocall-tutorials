4f1030f3e92f291e16e214156ca516b1b654657a
wanchaloem laoket <wanchaloem.laokeut@googlemail.com>
2023-04-07 17:37:24 +0700

    update

--- src/app/pages/simple-message/components/answer/answer.component.ts
+++ src/app/pages/simple-message/components/answer/answer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-answer',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AnswerComponent {
 
+  private _peer: RTCPeerConnection & { dc?: RTCDataChannel } = new RTCPeerConnection();
+  public offerData: string = '';
+  public answerData: string = '';
+  public isConnected = false;
+  public messageItems: { isMe: boolean, text: string }[] = [];
+
+  constructor(private _detect: ChangeDetectorRef) {
+    this._peer.onconnectionstatechange = ev => {
+      console.log(this._peer.connectionState);
+    }
+  }
+
+  onCreateAnswer() {
+    this._peer.ondatachannel = ev => {
+      this._peer.dc = ev.channel;
+      this._peer.dc.onopen = this._initializeChannelOpen.bind(this);
+      this._peer.dc.onmessage = this._initializeChannelMessage.bind(this);
+    };
+    this._peer.setRemoteDescription(JSON.parse(this.offerData));
+    this._peer.onicecandidate = ev => {
+      if (!ev.candidate) return;
+      this.answerData = JSON.stringify(this._peer.localDescription);
+      this._detect.detectChanges();
+    };
+    this._peer.createAnswer().then(answer => this._peer.setLocalDescription(answer));
+  }
+
+  onSendMessage(input: HTMLInputElement) {
+    const value = input.value.trim();
+    if (value) {
+      this.messageItems.push({ isMe: true, text: value });
+      this._peer.dc?.send(value);
+    }
+    input.value = '';
+    this._detect.detectChanges();
+  }
+
+  private _initializeChannelOpen() {
+    this.isConnected = true;
+    this._detect.detectChanges();
+  }
+
+  private _initializeChannelMessage(ev: MessageEvent<string>) {
+    this.messageItems.push({ isMe: false, text: ev.data });
+    this._detect.detectChanges();
+  }
 }
--- src/app/pages/simple-message/simple-message.module.ts
+++ src/app/pages/simple-message/simple-message.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { SimpleMessageRoutingModule } from './simple-message-routing.module';
 import { OfferComponent } from './components/offer/offer.component';
 import { AnswerComponent } from './components/answer/answer.component';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
     OfferComponent,
-    AnswerComponent
+    AnswerComponent,
   ],
   imports: [
     CommonModule,
-    SimpleMessageRoutingModule
+    SimpleMessageRoutingModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule,
+    FormsModule,
+    CardModule,
   ]
 })
 export default class SimpleMessageModule { }
