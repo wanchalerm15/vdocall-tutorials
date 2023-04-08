9ff2e067146059d36f1fc6911a47dcabe932e4d0
wanchaloem laoket <wanchaloem.laokeut@googlemail.com>
2023-04-08 15:08:21 +0700

    update

---
+++ front-end/src/app/pages/simple-message/components/chat/chat.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent {
+}
