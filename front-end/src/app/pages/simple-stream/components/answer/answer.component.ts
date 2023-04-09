6aa6bbb8afd262aa163012d671bdfaf3b494ae05
wanchaloem laoket <wanchaloem.laokeut@googlemail.com>
2023-04-09 15:17:17 +0700

    EP-5.5 การส่งข้อมูล Stream ผ่าน WebRTC ด้วย Ang

--- front-end/src/app/pages/simple-stream/components/answer/answer.component.ts
+++ front-end/src/app/pages/simple-stream/components/answer/answer.component.ts
@@ -12,6 +12,7 @@ export class AnswerComponent {
     private _message: MessageService,
     private _zone: NgZone,
   ) {
+    this._initalizeLoadDevices();
     this._peer.onconnectionstatechange = () => {
       this._zone.run(() => {
         this._message.add({ severity: 'info', summary: 'แจ้งเตือนการเชื่อมต่อ', detail: this._peer.connectionState });
@@ -32,13 +33,22 @@ export class AnswerComponent {
 
   private _peer: RTCPeerConnection & { dc?: RTCDataChannel } = new RTCPeerConnection();
 
+  localStream?: MediaStream;
+  devliceItem = {
+    videos: [] as MediaDeviceInfo[],
+    audios: [] as MediaDeviceInfo[]
+  };
+
+  videoDevice: string = '';
+  audioDevice: string = '';
+
   connected: boolean = false;
   offerData: string = '';
   answerData: string = '';
   step: number = 0;
   steps: MenuItem[] = [
-    { label: 'เปิดกล้อง' },
     { label: 'เก็บ Offer' },
+    { label: 'เปิดกล้อง' },
     { label: 'สร้าง Answer' },
     { label: 'คัดลอก Answer' },
   ];
@@ -50,18 +60,31 @@ export class AnswerComponent {
     this.step = 1;
   }
 
+  /** เปิดกล้องและไมค์ */
+  async openStream() {
+    try {
+      this.localStream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: { exact: this.videoDevice } },
+        audio: { deviceId: { exact: this.audioDevice } }
+      });
+      this.step = 2;
+    }
+    catch (ex: any) {
+      this._message.add({ severity: 'error', summary: 'เปิดกล้องไม่ได้', detail: ex.message });
+    }
+  }
+
   /** สร้างข้อมูล Answer */
   createAnswer() {
-    this._peer.ondatachannel = ev => {
-      this._peer.dc = ev.channel;
-      this._peer.dc.onmessage = ev => this._onChannelMessage(ev);
-    };
+    if (!this.localStream)
+      return this._message.add({ severity: 'warn', summary: 'แจ้งเตือน', detail: 'ไม่มีข้อมูล Stream' });
 
+    this.localStream.getTracks().forEach(track => this._peer.addTrack(track, this.localStream!));
     this._peer.onicecandidate = ev => {
       if (!ev.candidate) return;
       this._zone.run(() => {
         this.answerData = JSON.stringify(this._peer.localDescription);
-        this.step = 2;
+        this.step = 3;
       });
     };
 
@@ -69,11 +92,22 @@ export class AnswerComponent {
     this._peer.createAnswer().then(answer => this._peer.setLocalDescription(answer));
   }
 
-  /** เมื่ออีกเครื่องส่งข้อความมา */
-  private _onChannelMessage(ev: MessageEvent<string>) {
-    this._zone.run(() => {
-      console.log('Message: ', ev.data);
+  /** โหลดข้อมูล device กล้องและไมค์ */
+  private async _initalizeLoadDevices() {
+    const deviceItems = await navigator.mediaDevices.enumerateDevices();
+    deviceItems.forEach(item => {
+      switch (item.kind) {
+        case 'audioinput':
+          this.devliceItem.audios.push(item);
+          break;
+        case 'videoinput':
+          this.devliceItem.videos.push(item);
+          break;
+      }
     });
+
+    if (this.devliceItem.videos.length > 0) this.videoDevice = this.devliceItem.videos[0].deviceId;
+    if (this.devliceItem.audios.length > 0) this.audioDevice = this.devliceItem.audios[0].deviceId;
   }
 
 }
